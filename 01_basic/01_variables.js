e1fed4f782baa8b6685fc76dbac5fcbe470670b2
mishraashish084 <69692489+mishraashish084@users.noreply.github.com>
2024-01-03 22:59:37 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const  accountId=144553
+let accountEmail="mishraashish@123.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+//accountId=2;: this is constant datatype which can't be change
+var accountPassword="123"// not in more use because difficult to define in scope
+accountCity="Bengaluru"
+
+console.log(accountId)
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";// treat all js code as newer version
+//alert(3+3)// we are using nodejs,not browser
+
+let name="Ashish"//string
+let age=26//integer
+let isLoggedIn= false//boolean
+//null=> standalone value, ie. let a=null
+//undefined=>not provided value ie. let a;
+console.log(typeof isLoggedIn);
+console.log(typeof null);//object
