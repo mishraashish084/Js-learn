4b10ae3690064f7122ca125450eaa644d820c3b9
mishraashish084 <69692489+mishraashish084@users.noreply.github.com>
2024-01-04 00:00:55 +0000

    updateconcersion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score ="33ab"
-console.log(typeof score);
-console.log(typeof (score))
+// console.log(typeof score);
+// console.log(typeof (score))
 
 let value = Number(score)//typecasting
-console.log(typeof value);
-console.log(value)
+//console.log(typeof value);
+//console.log(value)
 
 
 //"33" =>33
@@ -14,6 +14,18 @@ console.log(value)
 //let isLoggedIn=1
 //let bool= Boolean(isLoggedIn)
 //console.log(bool)
-let isLoggedIn= null
-let bool=Boolean(isLoggedIn)
-console.log(bool)
+//let isLoggedIn= null
+//let bool=Boolean(isLoggedIn)
+//console.log(bool)
+//******************************Operations****************************** */
+let val=3
+let negval=-val;
+console.log(negval)
+let str1= "hello"
+let str2= " Ashish"
+let str3= str1 + str2;
+console.log(str3);
+console.log("1" + 2)
+console.log("1" + 2 +3)
+console.log(1 +2 + "3")
+console.log(+true)
